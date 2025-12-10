546e872ded07b63e118bb12d17ceab496170aa2e
Sergio García <3311638+serchtul@users.noreply.github.com>
2025-12-09 23:10:55 -0600

    add clojure lsps

--- .config/nvim/lua/kickstart/plugins/lsp.lua
+++ .config/nvim/lua/kickstart/plugins/lsp.lua
@@ -208,17 +208,12 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        clojure_lsp = {},
+        ['clj-kondo'] = {},
+        cljfmt = {},
         gopls = {},
         pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
-        svelte = {},
         emmet_language_server = {},
         lua_ls = {
           -- cmd = { ... },
