f275335220cef33cb38f17575b02ada310846713
Sergio García <3311638+serchtul@users.noreply.github.com>
2026-02-13 18:54:35 -0600

    add useful neotree keybindings

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -11,6 +11,9 @@ return {
   },
   cmd = 'Neotree',
   keys = {
+    { '<leader>ng', ':Neotree reveal float git_status<CR>', desc = '[N]eoTree Show [G]it Status', silent = true },
+    { '<leader>n<leader>', ':Neotree reveal float buffers<CR>', desc = '[N]eoTree Show Buffer Files', silent = true },
+    { '<leader>nn', ':Neotree reveal float<CR>', desc = 'NeoTree reveal', silent = true },
     { '\\', ':Neotree reveal float<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
--- .config/nvim/lua/kickstart/plugins/which-key.lua
+++ .config/nvim/lua/kickstart/plugins/which-key.lua
@@ -49,6 +49,7 @@ return {
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
         { '<leader>a', group = 'H[a]rpoon' },
+        { '<leader>n', group = '[N]eotree' },
       },
     },
   },
